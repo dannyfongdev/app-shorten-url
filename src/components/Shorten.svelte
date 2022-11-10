b4a2f756382dea5b57f1c64eca6c0d3f7da314b3
Danny Fong <danny.fong.dev@outlook.com>
2022-11-09 21:30:06 -0800

    Shorten functionality with Svelte

--- src/components/Shorten.svelte
+++ src/components/Shorten.svelte
@@ -1,23 +1,76 @@
+<script>
+  let urls = [];
+
+  $: totalUrls = urls.length;
+  let newId;
+  $: {
+    if (totalUrls === 0) {
+      newId = 1;
+    } else {
+      newId = Math.max(...urls.map((u) => u.id)) + 1;
+    }
+  }
+
+  let searchString = "";
+
+  const shorty = new Shorten();
+
+  function getShortUrl(url) {
+    shorty
+      .getShortUrl(url)
+      .then((result) => {
+        paintUrl(url, result);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function paintUrl(longUrl, shortUrl) {
+    urls = [
+      ...urls,
+      { id: newId, long: longUrl, short: shortUrl, isCopied: false },
+    ];
+    searchString = "";
+  }
+
+  function copyToClipboard(url) {
+    try {
+      navigator.clipboard.writeText(url)
+    } catch(err) {
+      console.log("error copying text")
+    }
+  }
+</script>
+
 <section class="section-shorten">
-    <div class="shorten">
-      <div class="shorten-bar">
-        <input id="shorten-string" type="text" placeholder="Shorten a link here...">
-        <!-- <p id="shorten-alert"><em>Please add a link</em></p> -->
-        <a href="#" class="btn btn-square">Shorten It!</a>
-      </div>
-      <div class="shorten-result">
-        <p class="long-link">https://www.frontendmentor.io</p>
-        <div>
-          <p class="short-link">https://rel.ink/k4Kyk</p>
-          <a href="#" class="btn btn-square">Copy</a>
-        </div>
-      </div>
-      <div class="shorten-result">
-        <p class="long-link">https://www.frontendmentor.io</p>
-        <div>
-          <p class="short-link">https://rel.ink/k4Kyk</p>
-          <a href="#" class="btn btn-square is-copied">Copied</a>
-        </div>
-      </div>
+  <div class="shorten">
+    <div class="shorten-bar">
+      <input
+        id="shorten-string"
+        type="text"
+        placeholder="Shorten a link here..."
+        bind:value={searchString}
+      />
+      <!-- <p id="shorten-alert"><em>Please add a link</em></p> -->
+      <a
+        href="/"
+        on:click|preventDefault={() => getShortUrl(searchString)}
+        class="btn btn-square">Shorten It!</a
+      >
     </div>
-  </section>
+    {#each urls as url (url.id)}
+      <div class="shorten-result">
+        <p class="long-link">{url.long}</p>
+        <div>
+          <p class="short-link">{url.short}</p>
+          <a
+            on:click|preventDefault={() => copyToClipboard(url.short)}
+            href="/"
+            class="btn btn-square"
+            class:is-copied={url.isCopied}
+            >{url.isCopied ? "Copied!" : "Copy"}</a
+          >
+        </div>
+      </div>
+    {/each}
+  </div>
+</section>
